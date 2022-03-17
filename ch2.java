598a9c899dae215b511215117a86b877bd45ef21
Byung-gwan <ansd43@gmail.com>
2022-03-18 00:55:09 +0900

    frist commit

---
+++ ch2.java
@@ -0,0 +1,12 @@
+class ch2 {
+    //print() - 출력 후에 줄바꿈을 안함
+    //println() - 출력 후에 자동으로 줄바꿈 
+    //문자열을 사용 할 때는 "", 상수로 나타 낼때는 숫자만 기입
+    public static void main(String args[]){
+        System.out.println(3+5);
+        System.out.println(3-4);
+        System.out.println(2+1);
+        System.out.println(4*2);
+        System.out.println(4/2);
+    }
+}
---
+++ chapter2.java
@@ -0,0 +1,15 @@
+class chapter2 {
+    public static void main(String args[]){
+        System.out.println("Hello, World");
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
+
+class EX2_2{
+    public static void main(String args[]){
+        System.out.println("Hello,World");
+        System.out.print("3+5=");
+        System.out.println(3+5);
+    }
+}
